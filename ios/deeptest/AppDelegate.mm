afe906baf371d212dd11aba126655490807e9e2a
Lev Kowalski <levkowalski@gmail.com>
2022-09-15 16:49:04 +0200

    Modify AppDelegate

--- ios/deeptest/AppDelegate.mm
+++ ios/deeptest/AppDelegate.mm
@@ -24,6 +24,17 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   std::shared_ptr<const facebook::react::ReactNativeConfig> _reactNativeConfig;
   facebook::react::ContextContainer::Shared _contextContainer;
 }
+
+// iOS 9.x or newer
+#import <React/RCTLinkingManager.h>
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
 @end
 #endif
 
